589109ea90e0f582eefd492856e3b738914cf58c
aramirezsuarez <aramirezsu@unal.edu.co>
2024-01-29 17:55:26 -0500

    Update Ingresar.py

--- pages/Ingresar.py
+++ pages/Ingresar.py
@@ -185,25 +185,3 @@ try:
         else:
             st.warning("Nombre de usuario no existe, por favor registrese")
 
-# Crear pie de pagina con los datos de contacto de los creadores
-footer = """
-<style>
-    .footer {
-        position: fixed;
-        left: 0;
-        bottom: 0;
-        width: 100%;
-        background-color: rgb(14, 17, 23);
-        color: black;
-        text-align: center;
-    }
-    .footer p {
-        color: white;
-    }
-</style>
-<div class="footer">
-    <p>App desarrollada por: <br />
-    Luis Fernando López Echeverri | Andres Felipe Ramirez Suarez <br />
-    Contactenos: <a href="#">lulopeze@unal.edu.co</a> | <a href="#">aramirezsu@unal.edu.co</a></p>
-</div>
-"""
