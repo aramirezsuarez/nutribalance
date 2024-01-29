ddf916af2d24e66450d901a18c435110f5fabe7b
aramirezsuarez <aramirezsu@unal.edu.co>
2024-01-29 17:04:17 -0500

    Update Registrarse.py

--- pages/Registrarse.py
+++ pages/Registrarse.py
@@ -6,140 +6,7 @@ import re
 from deta import Deta
 from datetime import datetime
 
-# Almacenamos la key de la base de datos en una constante
-DETA_KEY = "AXnEY5Eh_oygJSvkjrvwFsaEekozC7a8NeXpcAiAQ"
 
-# Creamos nuestro objeto deta para hacer la conexion a la DB
-deta = Deta(DETA_KEY)
-
-# Realizamos la conexion a la DB
-db = deta.Base("NutribalanceUsers")
-
-# Funcion para registrar usuarios en la DB
-def insertar_usuario(email, username, age, password):
-    """
-    Agrega un nuevo usuario a la Base de Datos.
-
-    Parameters:
-    - email (str): La dirección de correo electrónico del usuario.
-    - username (str): El nombre de usuario del usuario.
-    - age (int): La edad del usuario.
-    - height (float): La altura del usuario en centímetros.
-    - password (str): La contraseña del usuario.
-
-    Returns:
-    - None: La función no devuelve un valor explícito.
-
-    Note:
-    - La función agrega un nuevo usuario a la Base de Datos con la
-    información proporcionada. El nuevo usuario también tiene una lista vacía
-    llamada "food" que puede contener información
-    relacionada con las preferencias alimenticias del usuario en el futuro.
-    """
-    # Agregar un nuevo usuario a la Base de Datos con la
-    # información proporcionada
-    db.put({"key": email, "username": username, "age": age, "password": password,
-            "food": []})
-
-# Funcion que retorna los usuarios registrados
-def fetch_usuarios():
-    """
-    Obtiene un diccionario con los usuarios registrados desde la Base de Datos.
-
-    Returns:
-    - dict: Un diccionario que contiene información sobre los
-    usuarios registrados. Las claves del diccionario son las
-    identificaciones únicas de los usuarios.
-    Los valores son diccionarios que representan
-    la información detallada de cada usuario.
-
-    """
-    # Guardar los datos de la Base de Datos en 'users' y retornar su contenido
-    users = db.fetch()
-    return users.items
-
-# Funcion que retorna los emails de los usuarios registrados
-def get_emails_usuarios():
-    """
-    Obtiene una lista con los correos electrónicos de cada usuario
-    registrado en la Base de Datos.
-
-    Returns:
-    - list: Una lista que contiene los correos electrónicos de cada usuario.
-
-    """
-    # Guardar los datos de la Base de Datos en 'users'
-    users = db.fetch()
-
-    # Inicializar una lista para almacenar los correos electrónicos
-    emails = []
-
-    # Filtrar los correos electrónicos del diccionario de usuarios y
-    # agregarlos a la lista
-    for user in users.items:
-        emails.append(user["key"])
-
-    return emails
-
-# Funcion que retorna los nombres de usuario de los usuarios registrados
-def get_usernames_usuarios():
-    """
-    Obtiene una lista con los nombres de usuario de cada usuario
-    registrado en la Base de Datos.
-
-    Returns:
-    - list: Una lista que contiene los nombres de usuario de cada usuario.
-
-    """
-    # Guardar los datos de la Base de Datos en 'users'
-    users = db.fetch()
-
-    # Inicializar una lista para almacenar los nombres de usuario
-    usernames = []
-
-    # Filtrar los nombres de usuario del diccionario de usuarios y
-    # agregarlos a la lista
-    for user in users.items:
-        usernames.append(user["username"])
-
-    return usernames
-
-# Funcion que verifica si un email ingresado es valido
-def validar_email(email):
-    """
-    Verifica si el email ingresado es válido según patrones típicos.
-
-    Args:
-    - email (str): La dirección de correo electrónico a validar.
-
-    Returns:
-    - bool: True si el email es válido, False de lo contrario.
-
-    """
-    # Patrones típicos de un email válido
-    pattern = "^[a-zA-Z0_9-]+@[a-zA-Z0-9-]+\.[a-z]{1,3}$"
-    pattern1 = "^[a-zA-Z0-9-]+@[a-zA-Z0-9-]+\.[a-z]{1,3}+\.[a-z]{1,3}$"
-
-    # Verifica si el email ingresado coincide con alguno de los
-    # patrones definidos
-    if re.match(pattern, email) or re.match(pattern1, email):
-        return True
-    return False
-
-def validar_username(username):
-    """
-    Verifica si el username ingresado es válido según un patrón típico.
-
-    Args:
-    - username (str): El nombre de usuario a validar.
-
-    Returns:
-    - bool: True si el username es válido, False de lo contrario.
-
-    """
-    # Utiliza directamente el resultado de re.match,
-    # que es None si no hay coincidencia
-    return bool(re.match("^[a-zA-Z0-9]*$", username))
 
 # Formulario de registro con los datos que debe ingresar el usuario
 def registro():
@@ -210,31 +77,9 @@ def registro():
 
 # Manejo de posibles errores
 
-# Se almacenan los datos necesarios de la DB
-users = fetch_usuarios()
-emails = get_emails_usuarios()
-usernames = get_usernames_usuarios()
-passwords = [user["password"] for user in users]
 
 # Se crea el diccionario credentials necesario para el
 # funcionamiento del autenticador de cuentas
-credentials = {"usernames" : {}}
-for index in range(len(emails)):
-    credentials["usernames"][usernames[index]] = {"name" : emails[index],
-                                                "password" : passwords[index]}
-
-# Creacion del autenticador
-Authenticator = stauth.Authenticate(credentials, cookie_name="Streamlit",
-                                    key="cookiekey", cookie_expiry_days=3)
-
-# Crear boton de Cerrar sesion si la sesion fue iniciada
-if st.session_state["authentication_status"]:
-    Authenticator.logout("Cerrar sesion", location="sidebar")
-    st.write("Ya ha iniciado sesion")
-
-# Si la sesion no fue iniciada, ejecutar el formulario de registro
-else:
-    registro()
 
 
 # Crear pie de pagina con los datos de contacto de los creadores
