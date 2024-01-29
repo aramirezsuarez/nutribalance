3942b444420751fb78ead98ccb2a03586b7fb637
aramirezsuarez <aramirezsu@unal.edu.co>
2024-01-29 17:12:00 -0500

    Update registro_alimentos.py

--- pages/registro_alimentos.py
+++ pages/registro_alimentos.py
@@ -17,68 +17,6 @@ import requests
 import streamlit_authenticator as stauth
 from deta import Deta
 
-# Almacenamos la key de la base de datos en una constante
-DETA_KEY = "AXnEY5Eh_oygJSvkjrvwFsaEekozC7a8NeXpcAiAQ"
-
-# Creamos nuestro objeto deta para hacer la conexion a la DB
-deta = Deta(DETA_KEY)
-
-# Realizamos la conexion a la DB
-db = deta.Base("NutribalanceUsers")
-
-# Funcion que retorna los usuarios registrados
-def fetch_usuarios():
-    """Regresa un diccionario con los usuarios registrados"""
-    # guardamos los datos de la DB en users y retornamos su contenido
-    users = db.fetch()
-    return users.items
-
-# Funcion que retorna los emails de los usuarios registrados
-def get_emails_usuarios():
-    """Regresa una lista con los emails de cada usuario"""
-    # guardamos los datos de la DB en users
-    users = db.fetch()
-    emails = []
-    # filtramos los emails de la DB
-    for user in users.items:
-        emails.append(user["key"])
-    return emails
-
-# Funcion que retorna los nombres de usuario de los usuarios registrados
-def get_usernames_usuarios():
-    """Regresa una lista con los username de cada usuario"""
-    # guardamos los datos de la DB en users
-    users = db.fetch()
-    usernames = []
-    # filtramos los usernames de la DB
-    for user in users.items:
-        usernames.append(user["username"])
-    return usernames
-
-# Funcion que verifica si un email ingresado es valido
-def validar_email(email):
-    """Retorna True si el email ingresado es valido,
-    de lo contrario retorna False"""
-    # Patrones tipicos de un email valido
-    pattern = "^[a-zA-Z0_9-_]+@[a-zA-Z0_9-_]+\.[a-z]{1,3}$"
-    pattern1 = "^[a-zA-Z0_9-_]+@[a-zA-Z0_9-_]+\.[a-z]{1,3}+\.[a-z]{1,3}$"
-
-    # Verifica si el email ingresado coincide con algun patron definido
-    if re.match(pattern, email) or re.match(pattern1, email):
-        return True
-    return False
-
-# Funcion que verifica si un username ingresado es valido
-def validar_username(username):
-    """Retorna True si el username es valido, de lo contrario,
-    retorna False"""
-    # Se define el patron de un username tipico
-    pattern = "^[a-zA-Z0-9]*$"
-    # Se verifica si el username ingresado coincide con el patron tipico
-    if re.match(pattern, username):
-        return True
-    return False
-
 # Funcion para agregar valores caloricos a la DB registrados en un dia
 def add_food(email, new_food_data):
     """
