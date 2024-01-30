dded514c0d7e604da7196699e63a973b984ba4cc
aramirezsuarez <aramirezsu@unal.edu.co>
2024-01-29 20:50:55 -0500

    Update Ingresar.py

--- pages/Ingresar.py
+++ pages/Ingresar.py
@@ -81,47 +81,41 @@ def registro():
         st.warning("Debes aceptar los terminos y condiciones antes de poder registrarte")
 
 
-try:
-    users = fetch_users()
-    emails = []
-    usernames = []
-    passwords = []
+users = fetch_users()
+emails = []
+usernames = []
+passwords = []
 
-    for user in users:
-        emails.append(user['key'])
-        usernames.append(user['username'])
-        passwords.append(user['password'])
+for user in users:
+    emails.append(user['key'])
+    usernames.append(user['username'])
+    passwords.append(user['password'])
 
-    credentials = {'usernames': {}}
-    for index in range(len(emails)):
-        credentials['usernames'][usernames[index]] = {'name': emails[index], 'password': passwords[index]}
+credentials = {'usernames': {}}
+for index in range(len(emails)):
+    credentials['usernames'][usernames[index]] = {'name': emails[index], 'password': passwords[index]}
 
-    Authenticator = stauth.Authenticate(credentials, cookie_name='Streamlit', key='abcdef', cookie_expiry_days=4)
+Authenticator = stauth.Authenticate(credentials, cookie_name='Streamlit', key='abcdef', cookie_expiry_days=4)
 
-    email, authentication_status, username = Authenticator.login(':green[Login]', 'main')
+email, authentication_status, username = Authenticator.login(':green[Login]', 'main')
 
-    info, info1 = st.columns(2)
+info, info1 = st.columns(2)
 
-    if not authentication_status:
-        sign_up()
+if not authentication_status:
+    sign_up()
 
-    if username:
-        if username in usernames:
-            if authentication_status:
-                st.sidebar.subheader(f'Bienvenido {username}')
-                Authenticator.logout('Cerrar sesión', 'sidebar')
+if username:
+    if username in usernames:
+        if authentication_status:
+            st.sidebar.subheader(f'Bienvenido {username}')
+            Authenticator.logout('Cerrar sesión', 'sidebar')
 
-
-            elif not authentication_status:
-                with info:
-                    st.error('Contraseña o Usuario Incorrecto')
-            else:
-                with info:
-                    st.warning('Por favor introduzca sus credenciales')
+        elif not authentication_status:
+            with info:
+                st.error('Contraseña o Usuario Incorrecto')
         else:
             with info:
-                st.warning('Usuario no encontrado, Por favor registrese')
-
-
-except:
-    st.success('Inicio de sesion correcto')
+                st.warning('Por favor introduzca sus credenciales')
+    else:
+        with info:
+            st.warning('Usuario no encontrado, Por favor regístrese')
